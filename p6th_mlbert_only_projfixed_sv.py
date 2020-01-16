19e8f93a5b6fe7f791e2c3a7a01008895fb989c7
Hannes von Essen <hannes.von.essen@gmail.com>
2020-01-16 21:31:04 +0100

    More

--- p6th_mlbert_only_projfixed_sv.py
+++ p6th_mlbert_only_projfixed_sv.py
@@ -4,13 +4,13 @@ args = """--model_type bert
     --model_name_or_path bert-base-multilingual-cased
     --do_eval 
     --evaluate_during_training 
-    --train_file train_en_plus_proj_sv_no_impossible.json
+    --train_file train_only_projfixed_sv_no_impossible.json
     --predict_file dev_only_projfixed_sv_no_impossible.json
     --learning_rate 5e-6 
     --max_steps 160000 
     --max_seq_length 384 
     --doc_stride 128 
-    --output_dir 6th_ml_bert_en_plus_proj_sv
+    --output_dir 6th_ml_bert_only_projfixed_sv
     --per_gpu_eval_batch_size=128   
     --per_gpu_train_batch_size=3 
     --overwrite_cache  
